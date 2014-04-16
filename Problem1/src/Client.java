4a4262b4754b32a62e0895f9142dabde0f1afd28
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-04-16 03:46:20 -0300

    Working version of Problem1. Tested. Debugging still required.

--- Problem1/src/Client.java
+++ Problem1/src/Client.java
@@ -1,16 +1,230 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * @author Glavin Wiechert
- *
+ * 
  */
 public class Client {
 
 	/**
+	 * Socket connection.
+	 */
+	Socket socket;
+	/**
+	 * Input stream.
+	 */
+	ObjectInputStream input;
+	/**
+	 * Output stream.
+	 */
+	ObjectOutputStream output;
+
+	/**
+     * 
+     */
+	Scanner keyboard;
+
+	/**
+     * 
+     */
+	boolean isRunning;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param host
+	 * @param port
+	 */
+	public Client(String hostname, int port) {
+		// Connect to server
+		try {
+			this.socket = new Socket(hostname, port);
+			this.output = new ObjectOutputStream(this.socket.getOutputStream());
+			this.input = new ObjectInputStream(this.socket.getInputStream());
+			this.keyboard = new Scanner(System.in);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Start the Command-Line Interface
+	 */
+	public void start() {
+		isRunning = true;
+		try {
+			try {
+				// Send CONNECT
+				this.output.writeObject(new Protocol(Token.CONNECT, null));
+				
+				// Loop while running
+				while (isRunning && !this.socket.isClosed()) {
+
+					System.out.println("Ready for command.");
+					// Wait for READY message
+					Protocol p = (Protocol) input.readObject();
+					if (p.getToken().equals(Token.READY)) {
+						
+						System.out.println("Server is READY.");
+						
+						// Prompt for input
+						System.out.print("Enter command (PLUS, MINUS, MULTIPLY, DIVIDE, QUIT): ");
+						String op = keyboard.nextLine();
+						
+						// Perform respective operation
+						Token token = null;
+						switch (op) {
+							// Arithmetic Operations
+							case "PLUS":
+							case "plus":
+							case "add":
+							case "+": {
+								token = Token.PLUS;
+							}
+							break;
+							case "MINUS":
+							case "subtract":
+							case "minus":
+							case "-": {
+								token = Token.MINUS;								
+							}
+							break;
+							case "MULTIPLY":
+							case "times":
+							case "multiply":
+							case "*": {
+								token = Token.MULTIPLY;
+							}
+							break;
+							case "DIVIDE":
+							case "divide":
+							case "/": {
+								token = Token.DIVIDE;
+							}
+							break;
+							case "QUIT":
+							case "quit":
+							case "q": {
+								// Send QUIT
+								System.out.println("Quitting...");
+								if (!this.socket.isOutputShutdown()) 
+								{
+									this.output.writeObject(new Protocol(Token.QUIT, "QUIT"));
+								}
+								// Close connection
+								this.isRunning = false;
+								this.output.close();
+								this.input.close();
+								this.socket.close();
+							}
+							break;
+							default: {
+								throw new Exception("Unexpected Operator '"+op+"'.");
+							}
+						}
+						
+						// Confirm still running
+						// May have QUIT
+						if (isRunning) {
+							
+							// Send Token (Operation)
+							this.output.writeObject(new Protocol(token, null));
+							
+							// Read in two CONSTs
+							System.out.print("Enter first CONST1: ");
+							String c1 = keyboard.nextLine();
+							System.out.print("Enter second CONST1: ");
+							String c2 = keyboard.nextLine();
+							
+							// Send CONSTs as CONST1
+							this.output.writeObject(new Protocol(Token.CONST1, c1));
+							this.output.writeObject(new Protocol(Token.CONST1, c2));
+							
+							// Receive result CONST2
+							Protocol result = (Protocol) this.input.readObject();
+							
+							// Print result
+							System.out.println("Result: "+result.getArgument());
+							
+							// Continue and start over
+							
+						}
+						
+						
+					} else {
+						throw new UnexpectedTokenException(p.getToken(),
+								"READY");
+					}
+					
+				} // end run loop
+				
+			} catch (UnexpectedTokenException e) {
+				// Send ERROR
+				if (!this.socket.isOutputShutdown()) 
+				{
+					this.output.writeObject(new Protocol(Token.ERROR, e
+						.getMessage()));
+				}
+				// Close connection
+				this.output.close();
+				this.input.close();
+				this.socket.close();
+				//
+				e.printStackTrace();
+			} /* catch (UnexpectedArgumentException e) {
+				// Send ERROR
+				if (!this.socket.isOutputShutdown()) 
+				{
+					this.output.writeObject(new Protocol(Token.ERROR, e
+						.getMessage()));
+				}
+				// Close connection
+				this.output.close();
+				this.input.close();
+				this.socket.close();
+				//
+				e.printStackTrace();
+			} */ catch (ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * Start the client.
+	 * 
 	 * @param args
+	 *            The CLI arguments.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Client client;
+		if (args.length == 2) {
+			String hostname = args[0];
+			int port = Integer.parseInt(args[1]);
+
+			// Use arguments
+			client = new Client(hostname, port);
+		} else {
+			// Use Defaults
+			client = new Client("localhost", 6666);
+		}
+		client.start();
 	}
 
 }
--- Problem1/src/Server.java
+++ Problem1/src/Server.java
@@ -4,204 +4,227 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 /**
  * @author Glavin Wiechert
- *
+ * 
  */
-public class Server 
-{
+public class Server {
 
 	/**
 	 * Server socket.
 	 */
 	private ServerSocket serverSocket;
-	
+
 	/**
 	 * Client connected socket.
 	 */
 	private Socket socket;
-	
+
 	/**
 	 * 
 	 */
 	private ObjectOutputStream output;
-	
+
 	/**
 	 * 
 	 */
 	private ObjectInputStream input;
-	
+
 	/**
 	 * 
 	 */
 	private boolean isListening;
-	
+
 	/**
 	 * Constructor
 	 */
-	public Server() 
-	{
+	public Server() {
 		this.isListening = false;
 	}
-	
+
 	/**
 	 * Start the server, listening on the given port for client connections.
+	 * 
 	 * @param port Port to listen on.
 	 */
-	public void listen(int port) 
-	{
+	public void listen(int port) {
+		// Start listening
 		try {
-			// Start listening
-			this.serverSocket = new ServerSocket(port);	
-	        System.out.println("Listening on port "+port+".");
+			this.serverSocket = new ServerSocket(port);
+			System.out.println("Listening on port " + port + ".");
 			this.isListening = true;
-			
+	
 			// Wait for connections
-			while (isListening) 
-			{
-				// Get a Client Connection
-				this.socket = this.serverSocket.accept();
-                System.out.println("Found new connection.");
-                
-                // Get Object Streams 
-                this.output = new ObjectOutputStream(this.socket.getOutputStream());
-                this.input = new ObjectInputStream(this.socket.getInputStream());
-                
-            	try {
-	                // Wait for CONNECT message
-	                Protocol p = (Protocol) input.readObject();
-	                if (p.getToken().equals(Token.CONNECT)) {
-	                	
-	                	// Process Loop
-	                	while (socket.isConnected())
-	                	{
-		                	
-		                	// Tell Client that Server is ready. 
-		                	this.output.writeObject(new Protocol(Token.READY, null));
-		                	System.out.println("Ready for command.");
-		                	
-		                	// Wait for Arithmetic Operation or QUIT
-		                	p = (Protocol) input.readObject();
-		                	// Perform respective operation
-		                	switch (p.getToken()) {
-		                		//Arithmetic Operations
-			                	case PLUS: 
-			                	case MINUS: 
-			                	case MULTIPLY: 
-			                	case DIVIDE: 
-				                {
-				                	// Read two CONSTs
-				                	Protocol c1 = (Protocol) this.input.readObject();
-				                	Protocol c2 = (Protocol) this.input.readObject();
-				                	
-				                	// Validate CONSTs
-				                	Double v1;
-				                	Double v2;
-				                	try {
-					                	v1 = Double.parseDouble( c1.getArgument() );
-					                	v2 = Double.parseDouble( c2.getArgument() );
-				                	} catch (NumberFormatException e) {
-				                		throw new UnexpectedArgumentException("Argument(s) could not be parsed as doubles.");
-				                	}
-				                	
-				                	// Perform operation
-				                	Double result;
-				                	switch (p.getToken()) {
-					                	case PLUS:
-					                	{
-					                		result = v1 + v2;
-					                	}
-					                	break;
-					                	case MINUS:
-					                	{
-					                		result = v1 - v2;
-					                	}
-					                	break;
-					                	case MULTIPLY:
-					                	{
-					                		result = v1 * v2;
-					                	}
-					                	break;
-					                	case DIVIDE:
-					                	{
-					                		result = v1 / v2;
-					                	}
-					                	break;
-					                	default: 
-					                	{
-				                			// Unexpected operation token.
-				    						throw new UnexpectedTokenException(p.getToken(), "PLUS or MINUS or DIVIDE or MULTIPLY"); 								                		
-					                	}
-				                	}
-				                	
-				                	// Send back result
-				                	this.output.writeObject(new Protocol(Token.CONST2, String.valueOf(result)));
-				                	
-				                	// Continue, and loop to process next operation.
-				                	
-				                }
-			                	break;
-			                	case QUIT: 
-				                {
+			while (isListening) {
+	
+				try {
+					// Get a Client Connection
+					this.socket = this.serverSocket.accept();
+					System.out.println("Found new connection.");
+	
+					// Get Object Streams
+					this.output = new ObjectOutputStream(
+							this.socket.getOutputStream());
+					this.input = new ObjectInputStream(this.socket.getInputStream());
+	
+					try {
+						// Wait for CONNECT message
+						Protocol p = (Protocol) input.readObject();
+						if (p.getToken().equals(Token.CONNECT)) {
+	
+							// Process Loop
+							while (!socket.isClosed() && !socket.isOutputShutdown() && !socket.isInputShutdown() ) {
+								
+								// Tell Client that Server is ready.
+								this.output.writeObject(new Protocol(Token.READY,
+										null));
+								System.out.println("Ready for command.");
+	
+								// Wait for Arithmetic Operation or QUIT
+								System.out.println("Wait for operation.");
+								p = (Protocol) input.readObject();
+								// Perform respective operation
+								switch (p.getToken()) {
+								// Arithmetic Operations
+								case PLUS:
+								case MINUS:
+								case MULTIPLY:
+								case DIVIDE: {
+									// Read two CONSTs
+									Protocol c1 = (Protocol) this.input
+											.readObject();
+									Protocol c2 = (Protocol) this.input
+											.readObject();
+	
+									// Validate CONSTs
+									Double v1;
+									Double v2;
+									try {
+										v1 = Double.parseDouble(c1.getArgument());
+										v2 = Double.parseDouble(c2.getArgument());
+									} catch (NumberFormatException e) {
+										throw new UnexpectedArgumentException(
+												"Argument(s) could not be parsed as doubles.");
+									}
+	
+									// Perform operation
+									Double result;
+									switch (p.getToken()) {
+									case PLUS: {
+										result = v1 + v2;
+									}
+										break;
+									case MINUS: {
+										result = v1 - v2;
+									}
+										break;
+									case MULTIPLY: {
+										result = v1 * v2;
+									}
+										break;
+									case DIVIDE: {
+										result = v1 / v2;
+									}
+										break;
+									default: {
+										// Unexpected operation token.
+										throw new UnexpectedTokenException(
+												p.getToken(),
+												"PLUS or MINUS or DIVIDE or MULTIPLY");
+									}
+									}
+	
+									// Send back result
+									this.output.writeObject(new Protocol(
+											Token.CONST2, String.valueOf(result)));
+	
+									// Continue, and loop to process next operation.
+	
+								}
+									break;
+								case QUIT: {
 									// Send QUIT
-									this.output.writeObject(new Protocol(Token.QUIT, "QUIT"));
+									System.out.println("Connection quitting.");
+									if (!this.socket.isOutputShutdown()) {
+										this.output.writeObject(new Protocol(
+												Token.QUIT, "QUIT"));
+									}
 									// Close connection
 									this.output.close();
 									this.input.close();
 									this.socket.close();
-				                }
-			                	break;
-		                		default: 
-		                		{
-		                			// Unexpected operation token.
-		    						throw new UnexpectedTokenException(p.getToken(), "PLUS or MINUS or DIVIDE or MULTIPLY or QUIT");	                			
-		                		}
-		                	}
-		                	
-	                	}
-	                } else {
-						throw new UnexpectedTokenException(p.getToken(), "CONNECT");
-	                }
-				} catch (UnexpectedTokenException e) {
-					// Send ERROR
-					this.output.writeObject(new Protocol(Token.ERROR, e.getMessage()));
-					// Close connection
-					this.output.close();
-					this.input.close();
-					this.socket.close();
-					// 
+								}
+									break;
+								default: {
+									// Unexpected operation token.
+									throw new UnexpectedTokenException(
+											p.getToken(),
+											"PLUS or MINUS or DIVIDE or MULTIPLY or QUIT");
+								}
+								}
+	
+							}
+						} else {
+							throw new UnexpectedTokenException(p.getToken(),
+									"CONNECT");
+						}
+					} catch (UnexpectedTokenException e) {
+						// Send ERROR
+						if (!this.socket.isOutputShutdown()) {
+							this.output.writeObject(new Protocol(Token.ERROR, e
+									.getMessage()));
+						}
+						// Close connection
+						this.output.close();
+						this.input.close();
+						this.socket.close();
+						//
+						e.printStackTrace();
+					} catch (UnexpectedArgumentException e) {
+						// Send ERROR
+						if (!this.socket.isOutputShutdown()) {
+							this.output.writeObject(new Protocol(Token.ERROR, e
+									.getMessage()));
+						}
+						// Close connection
+						this.output.close();
+						this.input.close();
+						this.socket.close();
+						//
+						e.printStackTrace();
+					}
+	
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				catch (UnexpectedArgumentException e) {
-					// Send ERROR
-					this.output.writeObject(new Protocol(Token.ERROR, e.getMessage()));
-					// Close connection
-					this.output.close();
-					this.input.close();
-					this.socket.close();
-					// 
-					e.printStackTrace();
-				}
-			}
+				
+			} // End run loop
 			
-			
-		} catch (IOException e) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
 	}
-	
+
 	/**
+	 * Start the server application.
+	 * 
 	 * @param args
+	 *            Command-Line Arguments.
 	 */
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {
+		Server m = new Server();
+		if (args.length == 1) {
+			// Use args
+			m.listen(Integer.parseInt(args[0]));
+		} else {
+			// Default
+			m.listen(6666);
+		}
 	}
 
 }
